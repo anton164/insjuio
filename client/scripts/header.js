c01dcda29669ba8d935ba19f95e79eea03e313cd
Anton Abilov <anton_abilov@hotmail.com>
2017-04-02 14:40:57 +0200

    Tuned homepage messaging

--- client/scripts/header.js
+++ client/scripts/header.js
@@ -8,9 +8,13 @@ const Header = React.createClass({
             <img className="logo" src="images/insj.png" />
             <div className="header-content">
               <div className="header-content-inner">
-                <h1 id="homeHeading">Har du en idé?</h1>
-                <p>Har du en idé og lurer på hva du skal gjøre med den?</p>
-                <hr/>
+                <p>
+                  Pønsker du på en idé og lurer på hvordan du skal gå frem?
+                  <br />
+                  Vil du være med på å skape morgendagens tjenester?
+                </p>
+                <h1 id="homeHeading">Ta litt insj a</h1>
+                <br /><br /><br />
                 <a data-toggle="collapse" onClick={this.log} href="#ideacontact" className="btn btn-primary btn-xl page-scroll">Snakk med oss</a>
               </div>
             </div>
